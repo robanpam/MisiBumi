7c9f4ccd535e5a39bc9c62084d142923476ec052
robanpam <arikusumadewi3@gmail.com>
2024-08-06 09:05:17 +0700

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
+
         Carbon::setLocale('id');
 
         Gate::define('admin-gate', function(User $user){
